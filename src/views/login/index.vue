fc9d94eab854f1a643b9bf0bf246380ef2d3c1f4
Cherish <791832287@qq.com>
2022-04-25 18:16:06 +0800

    12312323

---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="card">
+      
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'login'
+}
+</script>
+<style lang="scss" scoped>
+.container{
+  height: 100vh;
+  width: 100%;
+  background-image: url('/img/bg.png');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-color: #B50029;
+}
+</style>
